c45b52d10a6341c34163f6e0854abb5603690250
cmwl <1357252390@qq.com>
2020-11-12 18:04:44 +0800

    用户授权

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -99,7 +99,8 @@ class WechatController extends Controller
 							  $appid = env("APP_Id");
 							  $redirect_uri = "http://www.shop.com/wx/code";
 						      $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appid."&redirect_uri=".$redirect_uri."&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
-							  file_get_contents($url);
+							  $get = file_get_contents($url);
+							  file_put_contents("ddd.log",$get);
 						}
 						$content = "";
 					   
