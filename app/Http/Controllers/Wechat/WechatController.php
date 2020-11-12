f3e8434cb528ab6d05457ebac2490ac3b6ebe68b
cmwl <1357252390@qq.com>
2020-11-12 17:53:01 +0800

    用户授权

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -97,9 +97,7 @@ class WechatController extends Controller
 						if($obj->EventKey == "http://www.wangshuxin.top/"){
 
 							  $appid = env("APP_Id");
-		                      $secret = env("SECRET");
 							  $redirect_uri = "http://www.shop.com/wx/code";
-
 						      $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=".$appid."&redirect_uri=".$redirect_uri."&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
 							  file_get_contents($url);
 						}
