6c2fdd74ae1280eab99378aca95a6b6623b6602e
cmwl <1357252390@qq.com>
2020-11-13 12:27:15 +0800

    自定义函数

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -63,5 +63,27 @@ use GuzzleHttp\Client;
 		  $data = $response->getBody();
 		  echo $data;
 	  }
+   
+	function checkSignature(){
+
+    $signature = $_GET["signature"];
+
+    $timestamp = $_GET["timestamp"];
+
+    $nonce = $_GET["nonce"];
+	
+    $token = env("WX_Token");
+
+    $tmpArr = array($token, $timestamp, $nonce);
+    sort($tmpArr, SORT_STRING);
+    $tmpStr = implode( $tmpArr );
+    $tmpStr = sha1( $tmpStr );
+    
+    if($tmpStr == $signature ){
+        return true;
+    }else{
+        return false;
+    }
+  }
 
 	
--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -11,7 +11,7 @@ class WechatController extends Controller
 {
     public function wechat(){
 	
-	        if ($this->checkSignature()) {
+	      
             $str = file_get_contents("php://input");
 
 			
@@ -294,29 +294,10 @@ class WechatController extends Controller
             }
             
             echo $this->xiaoxi($obj, $content);
-        }
+        
 	}
 
-	public function checkSignature(){
-    $signature = $_GET["signature"];
 
-    $timestamp = $_GET["timestamp"];
-
-    $nonce = $_GET["nonce"];
-	
-    $token = env("WX_Token");
-
-    $tmpArr = array($token, $timestamp, $nonce);
-    sort($tmpArr, SORT_STRING);
-    $tmpStr = implode( $tmpArr );
-    $tmpStr = sha1( $tmpStr );
-    
-    if($tmpStr == $signature ){
-        return true;
-    }else{
-        return false;
-    }
-  }
   public function code(){
        $code = $_GET['code'];
 
