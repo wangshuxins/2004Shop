51e08e146f5e575065667edfd6a72140e6d8b938
cmwl <1357252390@qq.com>
2020-11-15 19:20:41 +0800

    tijiao

--- app/Http/Controllers/Wechat/WechatController.php
+++ app/Http/Controllers/Wechat/WechatController.php
@@ -140,7 +140,7 @@ class WechatController extends Controller
 						 }else if($obj->EventKey=="wx_data"){
                                 echo $this->picture($obj);exit;
                          }else{
-							$address = ips();;
+							$address = ips();
 						    $city =  urlencode("$address");
                             $key = "2f3d1615c28f0a5bc54da5082c4c1c0c";
                             $url = "http://apis.juhe.cn/simpleWeather/query?city=".$city."&key=".$key;
